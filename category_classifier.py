d870e6b291abf732a4d0c081b8aa7033b2d0d4f3
zaphodbbrx <commondore.ne@gmail.com>
2018-09-27 10:00:08 +0300

    exf fix

--- category_classifier.py
+++ category_classifier.py
@@ -1,13 +1,5 @@
 import re
 import numpy as np
-from sklearn.svm import LinearSVC, SVC
-from sklearn.model_selection import train_test_split,cross_val_score
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import MaxAbsScaler
-from nltk.corpus import stopwords
-from sklearn.metrics import accuracy_score,f1_score, recall_score
-from IPython.display import clear_output
 from nltk import word_tokenize
 from scipy import sparse
 import pymystem3
@@ -19,18 +11,13 @@ class CategoryClassifier():
 
     def __init__(self,config_path):
         config = json.load(open(config_path))
-        with open(config['exf_path'],'rb') as f:
-            self.exf= pickle.load(f)
+        self.exf= extra_features_extractor().fit()
         with open(config['vect_path'],'rb') as f:
             self.vect= pickle.load(f)
         with open(config['num2title_path'],'rb') as f:
             self.num2title= pickle.load(f)
         with open(config['model_path'],'rb') as f:
             self.model= pickle.load(f)
-        self.decode_dict = {
-            1.0: 'scam',
-            0.0: 'no scam'
-        }
         self.mystem = pymystem3.Mystem()
 
     def __clean_message(self,text):
